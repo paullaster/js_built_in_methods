52e7207164cca9e12fbd824a7b7ed52d3cc132b3
paullaster <paullasterokoth98@gmail.com>
2022-09-22 07:23:36 +0300

    .

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const decodeUri = decodeURI(encodeUri);
  */
 const uri2 = 'https://www.example.com/submit?name=maaike van putten';
 const encodedUri = encodeURIComponent(uri2);
-//console.log(encodedUri); 
+console.log(encodedUri); 
 const decodedUri = decodeURIComponent(encodedUri);
 //console.log(decodedUri); 
 
